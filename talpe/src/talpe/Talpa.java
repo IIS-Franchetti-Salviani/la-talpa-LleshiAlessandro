732467750e597f3ab7091b933f2ab1a6ba561d6d
LleshiAlessandro <lleshi.alessandro@franchettisalviani.net>
2026-03-03 17:04:25 +0100

    ho finito la talpa ?
    nella talpa ho fatto dei metodi di supporto al metodo run, ed in fine ho sovrascritto il metodo run

--- talpe/src/talpe/Talpa.java
+++ talpe/src/talpe/Talpa.java
@@ -22,12 +22,12 @@ public class Talpa implements Runnable{
     private Random rdn = new Random();
     private int x = rdn.nextInt();
     
-    public Talpa(int timeOut) {
+    public Talpa(int timeOut){
         this.timeOut = timeOut;
         this.ritorna = timeOut;
     }
     
-    private void randomType() {
+    private void randomType(){
         int n = rdn.nextInt(3);
         switch (n) {
             case 0 -> type = TypeOfMole.GOLDEN;
@@ -37,13 +37,49 @@ public class Talpa implements Runnable{
         pointValue = type.getPointValue();
     }
     
+    public TypeOfMole getType(){
+        return type;
+    }
+    public int getPointValue(){
+        return pointValue;
+    }
     
+    
+
+    
+    //tutte le casistiche della talpa
+    public void appeare(){
+    hit = false;
+    //la talpa è uscita
+}
+    public void setHitTrue(){
+        if (!hit) {
+            hit = true;
+            //la talpa è stata colpita
+        }
+    }
+    public void controlEscape(){
+        if (!hit) {
+            //la talpa non è stata colpita :(((((
+        }
+    }
+    public boolean isHit(){
+        return hit;
+    }
     
     @Override
     public void run() {
-
-        
+    while (!Thread.currentThread().isInterrupted()){
+        try {
+                randomType();
+                appeare();
+                Thread.sleep(2000);
+                controlEscape();      
+                Thread.sleep(1000);
+            }
+        catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+        }
     }
-
-    
 }
